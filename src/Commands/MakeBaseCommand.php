eff21f6f3519bec6d6f24e4b1122eedb60fc54f8
delos <15066223705@163.com>
2021-02-20 14:06:28 +0800

    create base command

--- src/Commands/MakeBaseCommand.php
+++ src/Commands/MakeBaseCommand.php
@@ -2,145 +2,46 @@
 
 namespace Delosfei\Generator\Commands;
 
-use Delosfei\Generator\Makes\Base\MakerTraitBase;
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Input;
 use Illuminate\Console\Command;
 use Illuminate\Filesystem\Filesystem;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Input\InputArgument;
+use Illuminate\Support\Facades\File;
 
 class MakeBaseCommand extends Command
 {
-    use MakerTraitBase;
    // protected $signature = 'ds:code';
     protected $name = 'ds:base';
     protected $description = '生成基本代码';
-    protected $meta;
-    protected $files;
-    private $composer;
-    private $nameModel = "";
-
 
     public function __construct(Filesystem $files)
     {
         parent::__construct();
 
         $this->files = $files;
-        $this->composer = app()['composer'];
     }
 
 
     public function handle()
     {
-        $header = "scaffolding: {$this->getObjName("Name")}";
+        $header = "scaffolding: BaseCode";
         $footer = str_pad('', strlen($header), '-');
-        $dump = str_pad('>DUMP AUTOLOAD<', strlen($header), ' ', STR_PAD_BOTH);
 
         $this->line("\n----------- $header -----------\n");
-        $this->makeMeta();
-
-        // $this->call('migrate');
-        Artisan::call("migrate");
-
+        $this->copyFiles();
         $this->line("\n----------- $footer -----------");
-        $this->comment("----------- $dump -----------");
-
-        $this->composer->dumpAutoloads();
-
     }
-
-    protected function getArguments()
+    protected function path()
     {
-        return
-            [
-                ['name', InputArgument::REQUIRED, 'The name of the model. (Ex: Post)'],
-            ];
+        return base_path();
     }
-
-    protected function getOptions()
+    /**
+     * 复制文件
+     *
+     * @return void
+     */
+    protected function copyFiles()
     {
-        return
-            [
-                [
-                    'schema',
-                    's',
-                    InputOption::VALUE_REQUIRED,
-                    'Schema to generate scaffold files. (Ex: --schema="title:string")',
-                    null,
-                ],
-                [
-                    'ui',
-                    'ui',
-                    InputOption::VALUE_OPTIONAL,
-                    'UI Framework to generate scaffold. (Default bs4 - bootstrap 4)',
-                    'bs4',
-                ],
-                [
-                    'validator',
-                    'a',
-                    InputOption::VALUE_OPTIONAL,
-                    'Validators to generate scaffold files. (Ex: --validator="title:required")',
-                    null,
-                ],
-                [
-                    'localization',
-                    'l',
-                    InputOption::VALUE_OPTIONAL,
-                    'Localizations to generate scaffold files. (Ex. --localization="key:value")',
-                    null,
-                ],
-                [
-                    'lang',
-                    'b',
-                    InputOption::VALUE_OPTIONAL,
-                    'Language for Localization (Ex. --lang="en")',
-                    null,
-                ],
-                [
-                    'form',
-                    'f',
-                    InputOption::VALUE_OPTIONAL,
-                    'Use Illumintate/Html Form facade to generate input fields',
-                    false,
-                ],
-                [
-                    'prefix',
-                    'p',
-                    InputOption::VALUE_OPTIONAL,
-                    'Generate schema with prefix',
-                    false,
-                ],
-            ];
-    }
-
-    protected function makeMeta()
-    {
-        $this->meta['action'] = 'create';
-        $this->meta['table'] = $this->getObjName("table");//obsole to
-        $this->meta['Name'] = $this->getObjName('Name');
-        $this->meta['name'] = $this->getObjName('name');
-        $this->meta['schema'] = $this->option('schema');
-        $this->meta['prefix'] = ($prefix = $this->option('prefix')) ? "$prefix." : "";
+        File::copyDirectory(dirname(dirname(__DIR__)).'/Data/Base', $this->path());
     }
 
 
-    public function getMeta()
-    {
-        return $this->meta;
-    }
-
-    public function getObjName($config = 'Name')
-    {
-        $names = [];
-        $args_name = $this->argument('name');
-
-            $names['Name'] = \Str::singular(ucfirst($args_name));
-            $names['name'] = \Str::singular(strtolower(preg_replace('/(?<!^)([A-Z])/', '_$1', $args_name)));
-        if (!isset($names[$config])) {
-            throw new \Exception("Position name is not found");
-        };
-
-        return $names[$config];
-    }
 }

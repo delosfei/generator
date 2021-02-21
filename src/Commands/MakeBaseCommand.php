245356f0e85598024a570f7b7d998b6db304df3b
delos <15066223705@163.com>
2021-02-21 17:24:33 +0800

    以接口方式完成基础配置，实现前端自动路由，组件自动注册，axious请求等

--- src/Commands/MakeBaseCommand.php
+++ src/Commands/MakeBaseCommand.php
@@ -27,6 +27,7 @@ class MakeBaseCommand extends Command
 
         $this->line("\n----------- $header -----------\n");
         $this->copyFiles();
+        $this->info('以接口方式完成基础配置，实现前端自动路由，组件自动注册，axious请求等');
         $this->line("\n----------- $footer -----------");
     }
     protected function path()

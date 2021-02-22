508368f1035aa12dbb1705cde20d7db5adf7c651
delos <15066223705@163.com>
2021-02-22 08:57:40 +0800

    add tabler

--- src/Makes/Code/MakeController.php
+++ src/Makes/Code/MakeController.php
@@ -14,6 +14,7 @@ class MakeController
         $path = $this->getPath($name, 'controller-api');
         if ($this->files->exists($path))
         {
+
             return $this->scaffoldCommandObj->comment("x " . $path);
         }
 

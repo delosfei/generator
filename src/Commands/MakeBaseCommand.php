84bbef0cb5b1bf35f8ac29a63f76e0917d8d837c
delosfei <15066223705@163.com>
2021-04-17 11:57:12 +0800

    2021-04-17-7

--- src/Commands/MakeBaseCommand.php
+++ src/Commands/MakeBaseCommand.php
@@ -3,6 +3,8 @@
 namespace Delosfei\Generator\Commands;
 
 use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 
 class MakeBaseCommand extends Command
 {
@@ -25,6 +27,80 @@ class MakeBaseCommand extends Command
 
     }
 
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return
+            [
+                ['name', InputArgument::REQUIRED, 'The name of the model. (Ex: Post)'],
+            ];
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return
+            [
+                [
+                    'schema',
+                    's',
+                    InputOption::VALUE_REQUIRED,
+                    'Schema to generate scaffold files. (Ex: --schema="title:string")',
+                    null,
+                ],
+                [
+                    'ui',
+                    'ui',
+                    InputOption::VALUE_OPTIONAL,
+                    'UI Framework to generate scaffold. (Default bs4 - bootstrap 4)',
+                    'bs4',
+                ],
+                [
+                    'validator',
+                    'a',
+                    InputOption::VALUE_OPTIONAL,
+                    'Validators to generate scaffold files. (Ex: --validator="title:required")',
+                    null,
+                ],
+                [
+                    'localization',
+                    'l',
+                    InputOption::VALUE_OPTIONAL,
+                    'Localizations to generate scaffold files. (Ex. --localization="key:value")',
+                    null,
+                ],
+                [
+                    'lang',
+                    'b',
+                    InputOption::VALUE_OPTIONAL,
+                    'Language for Localization (Ex. --lang="en")',
+                    null,
+                ],
+                [
+                    'form',
+                    'f',
+                    InputOption::VALUE_OPTIONAL,
+                    'Use Illumintate/Html Form facade to generate input fields',
+                    false,
+                ],
+                [
+                    'prefix',
+                    'p',
+                    InputOption::VALUE_OPTIONAL,
+                    'Generate schema with prefix',
+                    false,
+                ],
+            ];
+    }
+
 
     public function handle()
     {
@@ -32,7 +108,7 @@ class MakeBaseCommand extends Command
         $footer = str_pad('', strlen($header), '-');
 
         $this->line("\n----------- $header -----------\n");
-        $this->info(dd($this->option('schema'))."\n");
+
         $this->info('name'.$this->argument('name')."\n");
         $this->comment('schema---'.$this->option('schema')."\n");
         $this->info('module_name'.$this->argument('module_name')."\n");
@@ -40,6 +116,6 @@ class MakeBaseCommand extends Command
 
         $this->line("\n----------- $footer-----------");
     }
-    
+
 
 }

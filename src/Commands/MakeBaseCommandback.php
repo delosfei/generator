572c2d88f7e98c419a3f10d6da7fe1d10475bd90
delosfei <15066223705@163.com>
2021-04-17 08:55:42 +0800

    2021-04-17-1

--- src/Commands/MakeBaseCommandback.php
+++ src/Commands/MakeBaseCommandback.php
@@ -8,14 +8,15 @@ use Illuminate\Support\Facades\File;
 
 class MakeBaseCommand extends Command
 {
-    protected $signature = 'ds:base';
-    protected $description = 'test command';
+   // protected $signature = 'ds:code';
+    protected $name = 'ds:base';
+    protected $description = '生成基本代码';
 
-    public function __construct()
+    public function __construct(Filesystem $files)
     {
         parent::__construct();
 
-
+        $this->files = $files;
     }
 
 
@@ -25,9 +26,22 @@ class MakeBaseCommand extends Command
         $footer = str_pad('', strlen($header), '-');
 
         $this->line("\n----------- $header -----------\n");
-
+        $this->copyFiles();
         $this->info('以接口方式完成基础配置，实现前端自动路由，组件自动注册，axious请求等');
         $this->line("\n----------- $footer -----------");
+    }
+    protected function path()
+    {
+        return base_path();
+    }
+    /**
+     * 复制文件
+     *
+     * @return void
+     */
+    protected function copyFiles()
+    {
+        File::copyDirectory(dirname(__DIR__).'/Data/Base', $this->path());
     }
 
 
